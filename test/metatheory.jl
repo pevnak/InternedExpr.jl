171733082d94f41d66973400712682beda263c03
Your Name <you@example.com>
2024-12-19 13:39:48 +0100

    have updated the testcases, have modified all_expand

--- test/metatheory.jl
+++ test/metatheory.jl
@@ -5,7 +5,8 @@ using Test
 using Serialization
 using Accessors
 
-using InternedExpr: NodeID, intern!
+using InternedExpr: NodeID, OnlyNode, intern!
+include("test_theory.jl")
 
 @testset "Rewriting rules" begin 
 
@@ -15,6 +16,8 @@ using InternedExpr: NodeID, intern!
         (:(sin(x + y)), @rule sin(~x + ~y) --> sin(~x)*cos(~y) + cos(~x)*sin(~y)),
         (:(a <= a), @rule ~a <= ~a --> 1),
         (:(100 + 10), @rule ~a::Number + ~b::Number => a + b),
+        (:(a && b), @rule ~a && ~b --> ~b && ~a),
+        (:(a == b), @rule ~a == ~b => a != 0 && b != 0 ? intern!(:($a - $b == 0)) : intern!(:($a == $b))),
     ]
 
 
@@ -23,7 +26,11 @@ using InternedExpr: NodeID, intern!
         c = r(ex)
         syc = r(symex)
         @test c isa NodeID
-        @test expr(c) == syc
+        if syc isa NodeID
+            @test c == syc
+        else
+            @test expr(c) == syc
+        end
     end
 end
 
@@ -34,18 +41,19 @@ function all_expand(node::NodeID, theory)
 
 end
 
-function all_expand(node::OnlyNode, theory)
+function all_expand(node::NodeID, theory)
     self = [r(node) for r in theory]
     self = filter(!isnothing, self)
 
+    node = InternedExpr.nc[][node]
     node == InternedExpr.nullnode && return(NodeID[])
     lefts = all_expand(node.left, theory)
-    rights = all_expand(node.rights, theory)
+    rights = all_expand(node.right, theory)
     lefts = isempty(lefts) ? [node.left] : lefts
     rights = isempty(rights) ? [node.right] : rights
     
     childs = map(Iterators.product(lefts, rights)) do (l, r)
-        intern!(OnlyNode(node.head, node.call, node.value, l, r))
+        intern!(OnlyNode(node.head, node.iscall, node.v, l, r))
     end |> vec
     return vcat(self, childs)
 end
@@ -58,4 +66,7 @@ end
 # c isa NodeID
 # expr(c) == syc
 
-
+ex = :((((((v0 - v1) + 15) / 8) * 8 + v1) - 4) - 116 <= ((((v0 + 16) - v2) + 6) / 135) * 135 + v2 && ((v3 - (v4 * 2 + v5)) + v4 * 2) + 64 <= (v3 - v5) + 64)
+symex = intern!(ex)
+r = theory[20]
+r(symex)
